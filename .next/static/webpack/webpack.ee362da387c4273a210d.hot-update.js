adf34b0114f02115efbd3d318a07461056c3fd46
Latricia <tricianickelberry@gmail.com>
2021-08-21 07:40:29 -0700

    updating routes

---
+++ .next/static/webpack/webpack.ee362da387c4273a210d.hot-update.js
@@ -0,0 +1,12 @@
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "158f5b9e3b7899858a99"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FL3dlYnBhY2svcnVudGltZS9nZXRGdWxsSGFzaCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7VUFBQSxvQ0FBb0MsK0JBQStCLEUiLCJmaWxlIjoic3RhdGljL3dlYnBhY2svd2VicGFjay5lZTM2MmRhMzg3YzQyNzNhMjEwZC5ob3QtdXBkYXRlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gZnVuY3Rpb24oKSB7IHJldHVybiBcIjE1OGY1YjllM2I3ODk5ODU4YTk5XCI7IH0iXSwic291cmNlUm9vdCI6IiJ9
--- components/Theader.js
+++ components/Theader.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Header, Menu } from 'semantic-ui-react';
+import {Link} from '../routes';
 
 const  Theader = () => {
     return(
 
-        <Menu>
-          
-          <Menu.Item> Crowd Coin</Menu.Item>
-            
+        <Menu style={{ marginTop: '10px'}}>
 
+          <link route= "/">
+            <a className="item">
+              Crowd Coin
+            </a>
+          </link>
+        
             <Menu.Menu position="right">
                
             <Menu.Item> Campaigns  </Menu.Item>
